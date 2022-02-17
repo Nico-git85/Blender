0e5ae563259f67388652c6147f7fac2223d3411b
Beleiu Cristina Nicoleta <61590793+Nico-git85@users.noreply.github.com>
2022-02-17 21:09:28 +0200

    mixt html and webGl

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
-const ip = require('internal-ip')
+const ip = require('ip')
 const portFinderSync = require('portfinder-sync')
 
 const infoColor = (_message) =>
@@ -14,6 +14,10 @@ module.exports = merge(
     {
         stats: 'errors-warnings',
         mode: 'development',
+        infrastructureLogging:
+        {
+            level: 'warn',
+        },
         devServer:
         {
             host: 'local-ip',
@@ -38,7 +42,7 @@ module.exports = merge(
             {
                 const port = devServer.options.port
                 const https = devServer.options.https ? 's' : ''
-                const localIp = ip.v4.sync()
+                const localIp = ip.address()
                 const domain1 = `http${https}://${localIp}:${port}`
                 const domain2 = `http${https}://localhost:${port}`
                 
--- src/script.js
+++ src/script.js
@@ -1,15 +1,53 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
-import testVertexShader from './shaders/test/vertex.glsl'
-import testFragmentShader from './shaders/test/fragment.glsl'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { gsap } from 'gsap'
+import { LatheGeometry, LogLuvEncoding, Raycaster } from 'three'
+
+/**
+ * Loaders
+ */
+
+let sceneReady = false
+const loadingBarElement = document.querySelector('.loading-bar')
+const loadingManager = new THREE.LoadingManager(
+    // Loaded
+    () =>
+    {
+        // Wait a little
+        window.setTimeout(() =>
+        {
+            // Animate overlay
+            gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 3, value: 0, delay: 1 })
+
+            // Update loadingBarElement
+            loadingBarElement.classList.add('ended')
+            loadingBarElement.style.transform = ''
+        }, 500)
+
+        window.setTimeout(() =>
+        {
+           sceneReady = true
+        }, 3000)
+    },
+
+    // Progress
+    (itemUrl, itemsLoaded, itemsTotal) =>
+    {
+        // Calculate the progress and update the loadingBarElement
+        const progressRatio = itemsLoaded / itemsTotal
+        loadingBarElement.style.transform = `scaleX(${progressRatio})`
+    }
+)
+const gltfLoader = new GLTFLoader(loadingManager)
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager)
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -18,47 +56,119 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Textures
+ * Overlay
  */
-const textureLoader = new THREE.TextureLoader()
-const flagTexture = textureLoader.load('/textures/flag-french.jpg')
-
-/**
- * Test mesh
- */
-// Geometry
-const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
-
-const count = geometry.attributes.position.count
-const randoms = new Float32Array(count)
-
-for(let i = 0; i < count; i++)
-{
-    randoms[i] = Math.random()
-}
-
-geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
-
-// Material
-const material = new THREE.ShaderMaterial({
-    vertexShader: testVertexShader,
-    fragmentShader: testFragmentShader,
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1)
+const overlayMaterial = new THREE.ShaderMaterial({
+    // wireframe: true,
+    transparent: true,
     uniforms:
     {
-        uFrequency: { value: new THREE.Vector2(10, 5) },
-        uTime: { value: 0 },
-        uColor: { value: new THREE.Color('orange') },
-        uTexture: { value: flagTexture }
-    }
+        uAlpha: { value: 1 }
+    },
+    vertexShader: `
+        void main()
+        {
+            gl_Position = vec4(position, 1.0);
+        }
+    `,
+    fragmentShader: `
+        uniform float uAlpha;
+
+        void main()
+        {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+        }
+    `
 })
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial)
+scene.add(overlay)
 
-gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX')
-gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY')
+/**
+ * Update all materials
+ */
+const updateAllMaterials = () =>
+{
+    scene.traverse((child) =>
+    {
+        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
+        {
+            // child.material.envMap = environmentMap
+            child.material.envMapIntensity = debugObject.envMapIntensity
+            child.material.needsUpdate = true
+            child.castShadow = true
+            child.receiveShadow = true
+        }
+    })
+}
 
-// Mesh
-const mesh = new THREE.Mesh(geometry, material)
-mesh.scale.y = 2 / 3
-scene.add(mesh)
+/**
+ * Environment map
+ */
+const environmentMap = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg'
+])
+
+environmentMap.encoding = THREE.sRGBEncoding
+
+scene.background = environmentMap
+scene.environment = environmentMap
+
+debugObject.envMapIntensity = 2.5
+
+/**
+ * Models
+ */
+gltfLoader.load(
+    '/models/DamagedHelmet/glTF/DamagedHelmet.gltf',
+    (gltf) =>
+    {
+        gltf.scene.scale.set(2.5, 2.5, 2.5)
+        gltf.scene.rotation.y = Math.PI * 0.5
+        scene.add(gltf.scene)
+
+        updateAllMaterials()
+    }
+)
+
+
+/**
+ * POints of interest
+ */
+const raycaster = new Raycaster();
+
+const points= [
+    {
+     position: new THREE.Vector3(1.55, 0.3, -0.6),
+     element: document.querySelector('.point-0')
+    },
+    {
+        position: new THREE.Vector3(0.5, 0.8, -1.6),
+        element: document.querySelector('.point-1')
+       },
+       {
+        position: new THREE.Vector3(1.6, -1.3, -0.7),
+        element: document.querySelector('.point-2')
+       }
+]
+
+
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+directionalLight.castShadow = true
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.normalBias = 0.05
+directionalLight.position.set(0.25, 3, - 2.25)
+scene.add(directionalLight)
 
 /**
  * Sizes
@@ -88,7 +198,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0.25, - 0.25, 1)
+camera.position.set(4, 1, - 4)
 scene.add(camera)
 
 // Controls
@@ -99,25 +209,63 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ReinhardToneMapping
+renderer.toneMappingExposure = 3
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update material
-    material.uniforms.uTime.value = elapsedTime
-
     // Update controls
     controls.update()
+ if(sceneReady)
+ {
+        //Go through each point
+    for(const point of points)
+    {
+         const screenPosition = point.position.clone()
+         screenPosition.project(camera)
+         
+         raycaster.setFromCamera(screenPosition, camera)
+         const intersects = raycaster.intersectObjects(scene.children, true)
+    
+         if(intersects.length === 0)
+         {
+             point.element.classList.add('visible')
+         }
+         else
+         {
+             const intersectionDistance = intersects[0].distance
+             const pointDistance = point.position.distanceTo(camera.position)
+    
+             if(intersectionDistance < pointDistance)
+             {
+                point.element.classList.remove('visible')
+             }
+            else
+            {
+                point.element.classList.add('visible')
+            }
+         }
+    
+         const translateX = screenPosition.x * sizes.width * 0.5
+         const translateY = - screenPosition.y * sizes.height * 0.5
+        point.element.style.transform = `translate(${translateX}px, ${translateY}px)`
+    
+    }
+ }
+ 
+
 
     // Render
     renderer.render(scene, camera)
